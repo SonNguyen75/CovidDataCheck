9dd3bcca09333673076179d33a9453ec3f3ae6c5
SKYDuclm <duclm@skylab.vn>
2022-04-12 08:36:32 +0700

    Mat-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,27 @@ import { TableComponent } from './table/table.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FilterComponent } from './filter/filter.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
     FilterComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
